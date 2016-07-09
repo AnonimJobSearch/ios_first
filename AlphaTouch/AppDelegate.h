5e996a66c4d14841218886a494145a7614d9c691
pavel <pavel>
2016-07-09 12:08:41 +0300

    Initial Commit

---
+++ AlphaTouch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AlphaTouch
+//
+//  Created by pavel on 7/9/16.
+//  Copyright © 2016 pavel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AlphaTouch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AlphaTouch
+//
+//  Created by pavel on 7/9/16.
+//  Copyright © 2016 pavel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
